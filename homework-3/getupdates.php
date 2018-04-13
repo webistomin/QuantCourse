b895dd095a0bfb513014a273dec968a776468b0a
webistomin <webistomin@gmail.com>
2018-04-13 14:45:38 +0300

    Начал делать бота

---
+++ homework-3/getupdates.php
@@ -0,0 +1,31 @@
+﻿<?php
+const TOKEN = '528147867:AAGrucTwA7olTmoQ51U2264dHuJE91m0Scs';
+
+$url = 'https://api.telegram.org/bot' . TOKEN . '/getUpdates';
+
+$lastupdate = 432423;
+
+$params = [
+    'offset' => $lastupdate + 1
+];
+
+$url = $url . '?' . http_build_query($params);
+
+
+$response = json_decode(file_get_contents($url), JSON_OBJECT_AS_ARRAY);
+
+
+if ($response['ok']) {
+    foreach ($response['result'] as $update) {
+        echo $text =  $update['message']['text'];
+        mail('webistomin@gmail.com', 'Test', $text)
+        ?><br><?php
+    }
+}
+
+
+
+
+
+//  var_dump($response);
+
